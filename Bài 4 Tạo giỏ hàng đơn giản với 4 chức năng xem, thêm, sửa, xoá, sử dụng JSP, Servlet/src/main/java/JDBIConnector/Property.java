864a37a3d04347e3ef7940315f47eb2845ae3a19
Mr T <167416986+thewin-1407@users.noreply.github.com>
2024-09-20 21:08:38 +0700

    Delete

--- Bài 4 Tạo giỏ hàng đơn giản với 4 chức năng xem, thêm, sửa, xoá, sử dụng JSP, Servlet/src/main/java/JDBIConnector/Property.java
+++
@@ -1,9 +0,0 @@
-package JDBIConnector;
-
-public class Property {
-    public  final  static String HOST ="localhost";
-    public  final  static String PORT ="3306";
-    public  final  static String DBNAME ="gao";
-    public  final  static String USER ="root";
-    public  final  static String PASSWORD ="";
-}
